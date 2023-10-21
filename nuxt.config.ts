d4e86c387ad430e35ad418da8537bf664d96bf61
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-10-21 15:29:47 +0300

    Tavsiye edilen ESLint yapılandırmasını ekler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/eslint-module"]
-})
+  modules: [
+    ['@nuxtjs/eslint-module', { /* module options */ }],
+  ]
+});
