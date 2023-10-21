70c3c836a279239cfedc41a60a0c3ccdf1526dab
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-10-21 15:33:39 +0300

    CSS kütüphanesi olarak Tailwindcss'i dahil eder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     ['@nuxtjs/eslint-module', { /* module options */ }],
-    '@pinia/nuxt'
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss'
   ]
 });
